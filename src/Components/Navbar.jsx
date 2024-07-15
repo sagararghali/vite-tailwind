bed035a375c45fa4d7298ef34e1d7d5a2d064db0
sagararghali <sagararghalimagar@gmail.com>
2024-07-15 14:34:14 +0545

    hover

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,16 @@ export default function Navbar(props) {
   return (<>
     <div className='bg-gray-500 rounded-lg flex justify-between'>
     <div className=' text-green-400 p-6  font-bold '>{props.title}</div>
-    <ul className='flex justify-between'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Blogs</li>
-        <li>Contact</li>
-        <li>Login</li>
+    <ul className='flex space-x-4 mt-6 '>
+        <li className='hover:text-red-400'>About</li>
+        <li className='hover:text-red-400'>Home</li>
+        <li className='hover:text-red-400'>Blogs</li>
+        <li className='hover:text-red-400'>Contact</li>
+        <li className='hover:text-red-400'>Login</li>
     </ul>
     </div>
+    <p>hello everyone my name is sagar arghali magar.</p>
     </>
   )
 }
+
