211b68f6363a387e48e10e506f06e2777f94e4b2
sagararghali <sagararghalimagar@gmail.com>
2024-07-15 14:24:22 +0545

    tailwind properties

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function Navbar() {
-  return (
-    <div className='bg-blue-500'>navbar title</div>
+export default function Navbar(props) {
+  return (<>
+    <div className='bg-gray-500 rounded-lg flex justify-between'>
+    <div className=' text-green-400 p-6  font-bold '>{props.title}</div>
+    <ul className='flex justify-between'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+        <li>Login</li>
+    </ul>
+    </div>
+    </>
   )
 }
