ac6837670279890858262e3588130f59f6b1beef
sagar magar <sagararghalimagar@gmail.com>
2024-07-25 08:41:43 -0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import { Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-    
     <Navbar title="Navbar"/>
-    <p>sagar</p>
     </>
   )
 }
+
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
   <>
     <div className='bg-gray-500 rounded-lg flex justify-between'>
-    <div className=' text-green-400 p-6  font-bold '>{props.title}</div>
-
+    <div className=" text-green-400 p-6  font-bold ">{props.title}</div>
     <ul className='flex space-x-6 mt-6 mr-8 relative'>
-        <a href="about.jsx"><li className='hover:text-red-400 '>About</li></a>
-        <a href=""><li className='hover:text-red-400 '>Home</li></a>
-        <a href=""><li className='hover:text-red-400 '>Blogs</li></a>
-        <a href=""><li className='hover:text-red-400 '>Contact</li></a>
-        <a href="login.jsx"><li className='hover:text-red-400 '>Login</li></a>
+        <Link to="/aboutpage"><li className='hover:text-red-400 '>About</li></Link>
+       <Link to="homepage"> <li className='hover:text-red-400 '>Home</li></Link>
+        <Link to="blogspage"><li className='hover:text-red-400 '>Blogs</li></Link>
+       <Link to="contactpage"> <li className='hover:text-red-400 '>Contact</li></Link>
+       <Link to="loginpage"> <li className='hover:text-red-400 '>Login</li></Link>
     </ul>
     </div>
     </>
